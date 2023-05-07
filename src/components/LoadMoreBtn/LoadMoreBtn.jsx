fd8a64670749a0216d96e5d4da0e3372ad9f2425
TarasVarvarych <varvarychtaras@gmail.com>
2023-05-07 14:42:53 +0300

    Update LoadMoreBtn.jsx

--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -1,8 +1,32 @@
+import styled from 'styled-components';
+
 function LoadMoreBtn({ onClick }) {
   return (
-    <button type="button" onClick={onClick}>
+    <LoadMoreButton type="button" onClick={onClick}>
       Load more
-    </button>
+    </LoadMoreButton>
   );
 }
 export default LoadMoreBtn;
+
+const LoadMoreButton = styled.button`
+  width: 200px;
+  height: 40px;
+  margin-top: 20px;
+  margin-left: auto;
+  margin-right: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  padding: 10px;
+  font-size: 18px;
+  background-color: #8db48e;
+  border: none;
+  border-radius: 5px;
+  &:hover {
+    background-color: #4d724d;
+    scale: 1.02;
+    cursor: pointer;
+  }
+`;
